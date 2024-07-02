ad86ece90ab3d426f4126731f5626e4ff33a5eb6
Anastasiia Myrka <myrkaa638@gmail.com>
2024-07-02 22:20:04 +0200

    fix MainAppEleven

--- main/java/com/telran/org/lessonfive/MainAppEleven.java
+++ main/java/com/telran/org/lessonfive/MainAppEleven.java
@@ -4,7 +4,7 @@ public class MainAppEleven {
 
     public static void main(String[] args) {
         int a = 1, b = 2, c = 2, d = 0;
-        if ((a < b) &&) b == c)){
+        if ((a < b) &&( b == c)){
             int sum = a + b + c + d;
             System.out.println(sum);
         }
